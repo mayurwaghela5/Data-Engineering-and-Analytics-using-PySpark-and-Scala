bcf70b40ab9aa121768712c0e4a1b607d3a36186
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-12 14:38:56 -0600

    added count function

--- itmd-521/labs/week-05/week05.scala
+++ itmd-521/labs/week-05/week05.scala
@@ -19,8 +19,9 @@ object week05 {
         val data_source_file=args(0)
         val infer_DF = spark.read.format("csv") .option("header", "true") .option("inferSchema", "true") .load(data_source_file)
         println("Infering the Schema In Scala")
-        infer_DF.show(15)
-        infer_DF.printSchema()
+        //println(infer_DF.show(15))
+        println(infer_DF.printSchema)
+        println("The number of records in this DataFrame is: "+ infer_DF.count())
         println("---------------------------------------------------------------------------------------------------------------")
         
         // Schema programmatically use StructFields
@@ -40,8 +41,10 @@ object week05 {
         ))
         val structure_divvy_DF=spark.read.schema(struct_schema).format("csv").option("header","true").option("structureSchema","true").load(data_source_file)
         println("Schema programmatically use StructFields")
-        structure_divvy_DF.show(10)
-        structure_divvy_DF.printSchema()
+        //structure_divvy_DF.show(10)
+        println(structure_divvy_DF.printSchema)
+        println("The number of records in this DataFrame is: "+ structure_divvy_DF.count())
+
 
         println("---------------------------------------------------------------------------------------------------------------")
 
@@ -50,8 +53,9 @@ object week05 {
         val schema_DDL= "trip_id INT, starttime STRING,stoptime STRING,bikeid INT,tripduration INT,from_station_id INT ,from_station_name STRING,to_station_id INT ,to_station_name STRING,usertype STRING,gender STRING,birthyear INT"
         val DDL_DF = (spark.read.schema(schema_DDL).format("csv")).option("header", "true").load(data_source_file)
         println("Attaching a schema via DLL and reading the csv")
-        DDL_DF.show()
-        DDL_DF.printSchema()
+        //DDL_DF.show()
+        print(DDL_DF.printSchema)
+        println("The number of records in this DataFrame is: "+ DDL_DF.count())
 
         println("---------------------------------------------------------------------------------------------------------------")
 
