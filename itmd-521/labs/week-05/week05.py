e88ee056cd1680ae1cbee298a56c7c208a9f91b3
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-13 21:28:00 -0600

    week05.py added

---
+++ itmd-521/labs/week-05/week05.py
@@ -0,0 +1,45 @@
+
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(-1)
+
+    spark = (SparkSession.builder.appName("week05").getOrCreate())
+    data_source_file = sys.argv[1]
+    
+    #inferring the Schema
+    infer_DF= (spark.read.format("csv").option("header","true").option("inferSchema","true").load(data_source_file))
+    infer_DF.show(n=15)
+    infer_DF.printSchema()
+    
+    # Schema programmatically use StructFields
+    structure_schema = StructType([StructField("trip_id", IntegerType()),
+        StructField("starttime", StringType()),
+        StructField("stoptime", StringType()),
+        StructField("bikeid", IntegerType()),
+        StructField("tripduration", IntegerType()),
+        StructField("from_station_id", IntegerType()),
+        StructField("from_station_name", StringType()),
+        StructField("to_station_id", IntegerType()),
+        StructField("to_station_name", StringType()),
+        StructField("usertype", StringType()),
+        StructField("gender", StringType()),
+        StructField("birthyear", IntegerType())])
+    
+    struc_Divvy_DF=(spark.read.schema(struct_schema).format("csv")).option("header","true").option("structureSchema","true").load(data_source_file)
+    struc_Divvy_DF.show()
+    structure_schema.printSchema()
+    
+    #attaching a schema via DDL
+    schema_DDL= "trip_id INT,starttime STRING,stoptime STRING,bikeid INT,tripduration INT,from_station_id INT,from_station_name STRING,to_station_id INT,to_station_name STRING,usertype STRING,gender STRING,birthyear INT"
+    DDL_Df= (spark.read.schema(schema_DDL).format("csv")).option("header", "true").load(data_source_file)
+    DDL_Df.show()
+    DDL_Df.printSchema()
+
+    
+    
+    
+    
