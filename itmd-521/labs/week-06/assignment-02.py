1752634be1cdb4abf284da756fb68265018373e3
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-16 09:18:08 -0600

    added questions

---
+++ itmd-521/labs/week-06/assignment-02.py
@@ -0,0 +1,13 @@
+#What were all the different types of fire calls in 2018?
+
+#What months within the year 2018 saw the highest number of fire calls?
+
+#Which neighborhood in San Francisco generated the most fire calls in 2018?
+
+#Which neighborhoods had the worst response times to fire calls in 2018?
+
+#Which week in the year in 2018 had the most fire calls?
+
+#Is there a correlation between neighborhood, zip code, and number of fire calls?
+
+#How can we use Parquet files or SQL tables to store this data and read it back?
