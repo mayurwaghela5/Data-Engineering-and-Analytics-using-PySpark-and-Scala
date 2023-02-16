cd5dd1c035a8d62554d31f29e736bd5f5d057a79
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-16 09:44:51 -0600

    created schema named fire_struct_schema

--- itmd-521/labs/week-06/assignment-02.py
+++ itmd-521/labs/week-06/assignment-02.py
@@ -11,3 +11,50 @@
 #Is there a correlation between neighborhood, zip code, and number of fire calls?
 
 #How can we use Parquet files or SQL tables to store this data and read it back?
+
+#CallNumber,UnitID,IncidentNumber,CallType,CallDate,WatchDate,CallFinalDisposition,AvailableDtTm,Address,
+# City,Zipcode,Battalion,StationArea,Box,OriginalPriority,Priority,FinalPriority,ALSUnit,CallTypeGroup,NumAlarms,
+# UnitType,UnitSequenceInCallDispatch,FirePreventionDistrict,SupervisorDistrict,Neighborhood,Location,RowID,Delay
+
+from select import select
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql import StringType, IntegerType, FloatType, BooleanType, StructField, StructType
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(-1)
+        
+    spark=(SparkSession.builder.appName("assignment-02").getOrCreate())
+    data_source_file=sys.argv[1]
+    
+    #create a scheme programmatically
+    fire_struct_schema = StructType([StructField('CallNumber', IntegerType(), True),
+        StructField('UnitID', StringType(), True),
+        StructField('IncidentNumber', IntegerType(), True),
+        StructField('CallType', StringType(), True),
+        StructField('CallDate', StringType(), True),
+        StructField('WatchDate', StringType(), True),
+        StructField('CallFinalDisposition', StringType(), True),
+        StructField('AvailableDtTm', StringType(), True),
+        StructField('Address', StringType(), True),
+        StructField('City', StringType(), True),
+        StructField('Zipcode', IntegerType(), True),
+        StructField('Battalion', StringType(), True),
+        StructField('StationArea', StringType(), True),
+        StructField('Box', StringType(), True),
+        StructField('OriginalPriority', StringType(), True),
+        StructField('Priority', StringType(), True),
+        StructField('FinalPriority', IntegerType(), True),
+        StructField('ALSUnit', BooleanType(), True),
+        StructField('CallTypeGroup', StringType(), True),
+        StructField('NumAlarms', IntegerType(), True),
+        StructField('UnitType', StringType(), True),
+        StructField('UnitSequenceInCallDispatch', IntegerType(), True),
+        StructField('FirePreventionDistrict', StringType(), True),
+        StructField('SupervisorDistrict', StringType(), True),
+        StructField('Neighborhood', StringType(), True),
+        StructField('Location', StringType(), True),
+        StructField('RowID', StringType(), True),
+        StructField('Delay', FloatType(), True)])
+    
