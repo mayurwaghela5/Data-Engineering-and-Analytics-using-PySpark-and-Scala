b13e383e34763af677367915ca609ea6fd180b96
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-22 11:48:04 -0600

    scala df via DDL created

--- itmd-521/labs/week-06/assignment02.scala
+++ itmd-521/labs/week-06/assignment02.scala
@@ -1,3 +1,9 @@
+//1. Detect failing devices with battery levels below a threshold.
+//2. Identify offending countries with high levels of CO2 emissions.
+//3. Compute the min and max values for temperature, battery level, CO2, and humidity.
+//4. Sort and group by average temperature, CO2, humidity, and country
+
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 
@@ -12,34 +18,10 @@ object assignment02 {
             System.exit(1)
         }
 
-        val fire_struct_schema=StructType(Array(StructField("CallNumber",IntegerType,false),
-        StructField("UnitID", StringType, false),
-        StructField("IncidentNumber", IntegerType, false),
-        StructField("CallType", StringType, false),
-        StructField("CallDate", StringType, false),
-        StructField("WatchDate", StringType, false),
-        StructField("CallFinalDisposition", StringType, false),
-        StructField("AvailableDtTm", StringType, false),
-        StructField("Address", StringType, false),
-        StructField("City", StringType, false),
-        StructField("Zipcode", IntegerType, false),
-        StructField("Battalion", StringType, false),
-        StructField("StationArea", StringType, false),
-        StructField("Box", StringType, false),
-        StructField("OriginalPriority", StringType, false),
-        StructField("Priority", StringType, false),
-        StructField("FinalPriority", IntegerType, false),
-        StructField("ALSUnit", BooleanType, false),
-        StructField("CallTypeGroup", StringType, false),
-        StructField("NumAlarms", IntegerType, false),
-        StructField("UnitType", StringType, false),
-        StructField("UnitSequenceInCallDispatch", IntegerType, false),
-        StructField("FirePreventionDistrict", StringType, false),
-        StructField("SupervisorDistrict", StringType, false),
-        StructField("Neighborhood", StringType, false),
-        StructField("Location", StringType, false),
-        StructField("RowID", StringType, false),
-        StructField("Delay", FloatType, false)
-        ))
+
+        //{"device_id": 1, "device_name": "meter-gauge-1xbYRYcj", "ip": "68.161.225.1", "cca2": "US", "cca3": "USA", "cn": "United States", "latitude": 38.000000, "longitude": -97.000000, "scale": "Celsius", "temp": 34, "humidity": 51, "battery_level": 8, "c02_level": 868, "lcd": "green", "timestamp" :1458444054093 }
+        
+        //creating df schema using DDL
+        val fire_struct_schema="device_id Long, device_name String, ip, cca2, cca3, cn, latitude, longitude, scale, temp, humidity, battery_level, c02_level, lcd, timestamp"
     }
 }
