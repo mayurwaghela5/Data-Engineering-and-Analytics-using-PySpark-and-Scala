1b037b7190df3d53a011e5737e82818ba45f615f
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-22 19:29:38 -0600

    computing min max for temp,humidity etc

--- itmd-521/labs/week-06/src/main/scala/chapter4/assignment02.scala
+++ itmd-521/labs/week-06/src/main/scala/chapter4/assignment02.scala
@@ -23,14 +23,17 @@ object assignment02 {
         }
         import spark.implicits._
         val iot_device_json=args(0)
-        //{"device_id": 1, "device_name": "meter-gauge-1xbYRYcj", "ip": "68.161.225.1", "cca2": "US", "cca3": "USA", "cn": "United States", "latitude": 38.000000, "longitude": -97.000000, "scale": "Celsius", "temp": 34, "humidity": 51, "battery_level": 8, "c02_level": 868, "lcd": "green", "timestamp" :1458444054093 }
-        
-        //creating df schema using DDL
-        //val fire_struct_schema="device_id Long, device_name String, ip String, cca2 String, cca3 String, cn String, latitude Double, longitude Double, scale String, temp Long, humidity Long, battery_level Long, c02_level Long, lcd String, timestamp Long"
         
         val ds = spark.read.json(iot_device_json).as[DeviceIoTData]
         ds.show(20, false)
-        //val fire_struct_schema_DF=spark.read.schema(fire_struct_schema).json(iot_device_json)
-        //fire_struct_schema_DF.show(true)
+
+        //3. Compute the min and max values for temperature, battery level, CO2, and humidity.
+
+        ds.agg(min("temp"),max("temp")).show()
+        ds.agg(min("battery_level"),max("battery_level")).show()
+        ds.agg(min("c02_level"),max("c02_level")).show()
+        ds.agg(min("humidity"),max("humidity")).show()
+
+        
     }
 }
