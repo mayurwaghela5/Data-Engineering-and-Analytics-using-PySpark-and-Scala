898a7d60878d62497185bfe9bd41f7ec21eb6797
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-22 22:24:19 -0600

    removed extra spaces

--- itmd-521/labs/week-06/assignment-02.py
+++ itmd-521/labs/week-06/assignment-02.py
@@ -1,21 +1,3 @@
-#What were all the different types of fire calls in 2018?
-
-#What months within the year 2018 saw the highest number of fire calls?
-
-#Which neighborhood in San Francisco generated the most fire calls in 2018?
-
-#Which neighborhoods had the worst response times to fire calls in 2018?
-
-#Which week in the year in 2018 had the most fire calls?
-
-#Is there a correlation between neighborhood, zip code, and number of fire calls?
-
-#How can we use Parquet files or SQL tables to store this data and read it back?
-
-#CallNumber,UnitID,IncidentNumber,CallType,CallDate,WatchDate,CallFinalDisposition,AvailableDtTm,Address,
-# City,Zipcode,Battalion,StationArea,Box,OriginalPriority,Priority,FinalPriority,ALSUnit,CallTypeGroup,NumAlarms,
-# UnitType,UnitSequenceInCallDispatch,FirePreventionDistrict,SupervisorDistrict,Neighborhood,Location,RowID,Delay
-
 from select import select
 import sys
 from pyspark.sql import SparkSession
