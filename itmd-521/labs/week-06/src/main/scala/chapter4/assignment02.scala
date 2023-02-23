e55e005a681de2e7f25be382e39ffba55cb0a4b6
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-22 20:57:19 -0600

    adding query 4

--- itmd-521/labs/week-06/src/main/scala/chapter4/assignment02.scala
+++ itmd-521/labs/week-06/src/main/scala/chapter4/assignment02.scala
@@ -1,7 +1,5 @@
 //1. Detect failing devices with battery levels below a threshold.
 
-      
-//4. Sort and group by average temperature, CO2, humidity, and country
 
 package main.scala.chapter4
 import org.apache.spark.sql.SparkSession
@@ -29,9 +27,9 @@ object assignment02 {
         val ds = spark.read.json(iot_device_json).as[DeviceIoTData]
         ds.show(20, false)
 
-        val max_co2=ds.agg(max("c02_level"))
+        //val max_co2=ds.agg(max("c02_level"))
         //2. Identify offending countries with high levels of CO2 emissions.
-        ds.select($"cn",$"c02_level").distinct().where(ds("c02_level")=="max_co2").show(70,false)
+        //ds.select($"cn",$"c02_level").distinct().where(ds("c02_level")=="max_co2").show(70,false)
 
         //3. Compute the min and max values for temperature, battery level, CO2, and humidity.
         //ANSWER: Min Temperature: 10, Max Temperature: 34
@@ -44,6 +42,12 @@ object assignment02 {
         ds.agg(min("c02_level"),max("c02_level")).show(false)
         ds.agg(min("humidity"),max("humidity")).show(false)
 
+
+        //4. Sort and group by average temperature, CO2, humidity, and country
+
+        val dsAvg=ds.select("cca3","temp","c02_level","humidity").groupBy("cca3").avg()
+                    .sort($"avg(temp)".desc,$"avg(c02_level)".desc ,$"avg(humidity)".desc).as("averages")
+        display(dsAvg)
         
     }
 }
