7022ce279316ff3d4a99e7b86c2d34757f4f0c5b
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-23 10:28:19 -0600

    added main in py file

---
+++ itmd-521/labs/week-07/assignment-03.py
@@ -0,0 +1,11 @@
+import sys
+from pyspark.sql import SparkSession
+from pyspark.sql import *
+from pyspark.sql.types import StructType, StructField, IntegerType,StringType,BooleanType,FloatType
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(-1)
+        
+    spark=(SparkSession.builder.appName("assignment-03").getOrCreate())
