362868884c953648cf544bdde1235787be4eb559
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-24 12:47:49 -0600

    printing schema for py

--- itmd-521/labs/week-07/assignment-03.py
+++ itmd-521/labs/week-07/assignment-03.py
@@ -10,4 +10,14 @@ if __name__ == "__main__":
         
     spark=(SparkSession.builder.appName("assignment-03").getOrCreate())
     data_source_file=sys.argv[1]
+    #date,delay,distance,origin,destination
+    #schema_ddl="date,delay,distance,origin,destination"
+    
+    
+    df=(spark.read.format("csv")
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .load(data_source_file))
+    df.printSchema()
+    #df.createOrReplaceTempView("us_delay_flights_tbl")
     
