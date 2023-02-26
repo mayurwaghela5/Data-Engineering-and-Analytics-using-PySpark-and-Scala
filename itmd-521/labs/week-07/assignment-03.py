c2e438676bc882d172db6053a3dda807e829564e
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-25 18:04:06 -0600

    edited pyspark query 3

--- itmd-521/labs/week-07/assignment-03.py
+++ itmd-521/labs/week-07/assignment-03.py
@@ -2,7 +2,7 @@ import sys
 from pyspark.sql import SparkSession
 from pyspark.sql import *
 from pyspark.sql.types import StructType, StructField, IntegerType,StringType,BooleanType,FloatType
-from pyspark.sql.functions import expr
+from pyspark.sql.functions import expr,col
 
 
 if __name__ == "__main__":
@@ -67,7 +67,7 @@ if __name__ == "__main__":
                                      WHEN delay > 60 AND delay < 120 THEN 'Short Delays' \
                                      WHEN delay > 0 and delay < 60 THEN 'Tolerable Delays' \
                                      WHEN delay = 0 THEN 'No Delays' \
-                                     ELSE 'Early' END AS Flight_Delays")).sort('origin','delay',ascending=False).show(10)
+                                     ELSE 'Early' END AS Flight_Delays")).sort(col('origin').desc,col('delay').desc).show(10)
     
     
     
