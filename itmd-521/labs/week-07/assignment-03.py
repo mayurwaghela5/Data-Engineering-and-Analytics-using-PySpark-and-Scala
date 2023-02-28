26bbee97925f05079dd7cd72a6109f67eefac62f
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-02-28 10:40:24 -0600

    edited path

--- itmd-521/labs/week-07/assignment-03.py
+++ itmd-521/labs/week-07/assignment-03.py
@@ -97,9 +97,13 @@ if __name__ == "__main__":
     df3 = (spark.read.schema(schema_ddl).format("csv")).option("header", "true").load(data_source_file)
     
     #Using a DataFrameWriter, write the content out as JSON
-    (df3.write.format("json").mode("overwrite").option("compression", "none").save("/home/vagrant/mwaghela/itmd-521/labs/week-07/spark-warehouse/df_json_withoutsnappy"))
+    (df3.write.format("json").mode("overwrite").option("compression", "none").save("./spark-warehouse/df_json_withoutsnappy"))
     
     #(df3.write.format("json").mode("overwrite").option("compression", "snappy").save("/home/vagrant/mwaghela/itmd-521/labs/week-07/spark-warehouse/df_json_withsnappy"))
+    
+    
+    #Using a DataFrameWriter, write the content out as PARQUET
+    #(df.write.format("parquet").mode("overwrite").option("compression", "snappy").save("/home/vagrant/mwaghela/itmd-521/labs/week-07/spark-warehouse/df_json_withParquet"))
     
     
     
