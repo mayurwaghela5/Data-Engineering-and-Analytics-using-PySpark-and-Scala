2933af6c2a910a1ce36852b5db57c61afd72f6d7
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-04-25 10:00:33 -0500

    added 40-csv and 40-parquet read and print schema

---
+++ itmd-521/final/part-two/MW-minio-read-40.py
@@ -0,0 +1,79 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import to_date
+
+# Removing hard coded password - using os module to import them
+import os
+import sys
+
+# Required configuration to load S3/Minio access credentials securely - no hardcoding keys into code
+conf = SparkConf()
+conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.3')
+conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
+
+conf.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
+conf.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
+conf.set("spark.hadoop.fs.s3a.endpoint", "http://minio1.service.consul:9000")
+conf.set("fs.s3a.path.style.access", "true")
+conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+conf.set("fs.s3a.connection.ssl.enabled", "false")
+
+# Create SparkSession Object - tell the cluster the FQDN of the host system)
+spark = SparkSession.builder.appName("MW part-two/minio-read-40").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
+
+# Read the csv datatype into a DataFrame
+csvdf = spark.read.csv('s3a://mwaghela/40-csv').cache()
+
+splitDF = csvdf.withColumn('WeatherStation', csvdf['_c0'].substr(5, 6)) \
+.withColumn('WBAN', csvdf['_c0'].substr(11, 5)) \
+.withColumn('ObservationDate',to_date(csvdf['_c0'].substr(16,8), 'yyyyMMdd')) \
+.withColumn('ObservationHour', csvdf['_c0'].substr(24, 4).cast(IntegerType())) \
+.withColumn('Latitude', csvdf['_c0'].substr(29, 6).cast('float') / 1000) \
+.withColumn('Longitude', csvdf['_c0'].substr(35, 7).cast('float') / 1000) \
+.withColumn('Elevation', csvdf['_c0'].substr(47, 5).cast(IntegerType())) \
+.withColumn('WindDirection', csvdf['_c0'].substr(61, 3).cast(IntegerType())) \
+.withColumn('WDQualityCode', csvdf['_c0'].substr(64, 1).cast(IntegerType())) \
+.withColumn('SkyCeilingHeight', csvdf['_c0'].substr(71, 5).cast(IntegerType())) \
+.withColumn('SCQualityCode', csvdf['_c0'].substr(76, 1).cast(IntegerType())) \
+.withColumn('VisibilityDistance', csvdf['_c0'].substr(79, 6).cast(IntegerType())) \
+.withColumn('VDQualityCode', csvdf['_c0'].substr(86, 1).cast(IntegerType())) \
+.withColumn('AirTemperature', csvdf['_c0'].substr(88, 5).cast('float') /10) \
+.withColumn('ATQualityCode', csvdf['_c0'].substr(93, 1).cast(IntegerType())) \
+.withColumn('DewPoint', csvdf['_c0'].substr(94, 5).cast('float')) \
+.withColumn('DPQualityCode', csvdf['_c0'].substr(99, 1).cast(IntegerType())) \
+.withColumn('AtmosphericPressure', csvdf['_c0'].substr(100, 5).cast('float')/ 10) \
+.withColumn('APQualityCode', csvdf['_c0'].substr(105, 1).cast(IntegerType())).drop('_c0')
+
+splitDF.printSchema()
+splitDF.show(10)
+
+parquetdf= spark.read.parquet('s3a://mwaghela/40-parquet')
+
+splitDF2 = parquetdf.withColumn('WeatherStation', parquetdf['_c0'].substr(5, 6)) \
+.withColumn('WBAN', parquetdf['_c0'].substr(11, 5)) \
+.withColumn('ObservationDate',to_date(parquetdf['_c0'].substr(16,8), 'yyyyMMdd')) \
+.withColumn('ObservationHour', parquetdf['_c0'].substr(24, 4).cast(IntegerType())) \
+.withColumn('Latitude', parquetdf['_c0'].substr(29, 6).cast('float') / 1000) \
+.withColumn('Longitude', parquetdf['_c0'].substr(35, 7).cast('float') / 1000) \
+.withColumn('Elevation', parquetdf['_c0'].substr(47, 5).cast(IntegerType())) \
+.withColumn('WindDirection', parquetdf['_c0'].substr(61, 3).cast(IntegerType())) \
+.withColumn('WDQualityCode', parquetdf['_c0'].substr(64, 1).cast(IntegerType())) \
+.withColumn('SkyCeilingHeight', parquetdf['_c0'].substr(71, 5).cast(IntegerType())) \
+.withColumn('SCQualityCode', parquetdf['_c0'].substr(76, 1).cast(IntegerType())) \
+.withColumn('VisibilityDistance', parquetdf['_c0'].substr(79, 6).cast(IntegerType())) \
+.withColumn('VDQualityCode', parquetdf['_c0'].substr(86, 1).cast(IntegerType())) \
+.withColumn('AirTemperature', parquetdf['_c0'].substr(88, 5).cast('float') /10) \
+.withColumn('ATQualityCode', parquetdf['_c0'].substr(93, 1).cast(IntegerType())) \
+.withColumn('DewPoint', parquetdf['_c0'].substr(94, 5).cast('float')) \
+.withColumn('DPQualityCode', parquetdf['_c0'].substr(99, 1).cast(IntegerType())) \
+.withColumn('AtmosphericPressure', parquetdf['_c0'].substr(100, 5).cast('float')/ 10) \
+.withColumn('APQualityCode', parquetdf['_c0'].substr(105, 1).cast(IntegerType())).drop('_c0')
+
+
+# The coalese() function
+# https://spark.apache.org/docs/latest/api/python/reference/pyspark.sql/api/pyspark.sql.DataFrame.coalesce.html
+# This will collapse your DataFrame into a single partition
+#writeDF = splitDF.coalesce(1)
+splitDF2.printSchema()
+splitDF2.show(10)
