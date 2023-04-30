bc4a695647a6653a34de8872b164cfa72597aed7
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-04-29 21:07:32 -0500

    part four queries

--- itmd-521/final/part-four/part-four-1.py
+++ itmd-521/final/part-four/part-four-1.py
@@ -1,16 +1,20 @@
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
-from pyspark.sql.types import IntegerType,StringType,DateType, FloatType, StructType, StructField
+from pyspark.sql.types import IntegerType,StructType, StructField,DateType, FloatType, DoubleType, StringType
 from pyspark.sql.functions import to_date
 from pyspark.sql.functions import col
 from pyspark.sql.functions import desc
-from pyspark.sql.functions import year,month,avg
+from pyspark.sql.functions import year
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
+from pyspark.sql.functions import year, month, col,avg
 
 # Removing hard coded password - using os module to import them
 import os
 import sys
+
+#defining file to save answer
+queryAnswerFile = "s3a://mgowda2/MDG-part-four-answers-parquet"
 
 # Required configuration to load S3/Minio access credentials securely - no hardcoding keys into code
 conf = SparkConf()
@@ -24,7 +28,9 @@ conf.set("fs.s3a.path.style.access", "true")
 conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 conf.set("fs.s3a.connection.ssl.enabled", "false")
 
-schema = StructType([StructField('WeatherStation', StringType(), True),
+spark = SparkSession.builder.appName("MW part Four 1").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
+
+structSchema = StructType([StructField('WeatherStation', StringType(), True),
 StructField('WBAN', StringType(), True),
 StructField('ObservationDate',  DateType(), True),
 StructField('ObservationHour', IntegerType(), True),
@@ -44,48 +50,46 @@ StructField('DPQualityCode', IntegerType(), True),
 StructField('AtmosphericPressure', FloatType(), True),
 StructField('APQualityCode', IntegerType(), True)])
 
-spark = SparkSession.builder.appName("MW part Four").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
+parquetdf = spark.read.parquet("s3a://mwaghela/60-parquet", header=True, schema=structSchema)   
+parquetdf.createOrReplaceTempView("sqlView")
 
-# Read the parquet datatype into a DataFrame
 
-parquetdf = spark.read.parquet("s3a://mwaghela/60-parquet", header=True, schema=schema)   
-parquetdf.createOrReplaceTempView("parquetdf_view")
-
-# Count the number of records
-Number_of_records = spark.sql(""" SELECT year(ObservationDate) As Year, count(*)
-                                    FROM parquetdf_view
+#Count the number of records
+countOfRecords = spark.sql(""" SELECT year(ObservationDate) As Year, count(*)
+                                    FROM sqlView
                                     WHERE Month(ObservationDate) = '2'
                                     group by Year
                                     order by Year desc;
                                  """
                                 )
-Number_of_records.show(20)
-     
-# Average air temperature
-average_air_temp = spark.sql("""  SELECT year(ObservationDate) As Year, AVG(AirTemperature) AS AvgAirTemp
-                                FROM parquetdf_view
+countOfRecords.show(20)
+
+#Average air temperature for month of February
+averageAirTemp = spark.sql("""  SELECT year(ObservationDate) As Year, AVG(AirTemperature) AS AvgAirTemperature
+                                FROM sqlView
                                 WHERE Month(ObservationDate) = '2'
                                 AND AirTemperature < 999 AND AirTemperature > -999
                                 group by Year
                                 order by Year desc;
                                 """
                                 )
-average_air_temp.show(20)
-                                   
-# Median air temperature
-median_air_temp = parquetdf.approxQuantile('AirTemperature', [0.5], 0.25)
-print(f"Median Air_Temperature:{median_air_temp}")
+averageAirTemp.show(20)
 
-#Standard_Deviation of Air temperature
-Std_Dev_Air_Temp = spark.sql(""" SELECT year(ObservationDate) As Year, std(AirTemperature) as Standard_deviation
-                                    FROM parquetdf_view
+                                   
+#Median air temperature for month of February
+medianAirTemp = parquetdf.approxQuantile('AirTemperature', [0.5], 0.25)
+print(f"Median air temmp:{medianAirTemp}")
+
+#Standard Deviation of air temperature for month of February
+standardAirTemp = spark.sql(""" SELECT year(ObservationDate) As Year, std(AirTemperature) as Standard_deviation
+                                    FROM sqlView
                                     WHERE Month(ObservationDate) = '2'
                                     AND AirTemperature < 999 AND AirTemperature > -999
                                     group by Year
                                     order by Year desc;
                                  """
                                 )
-Std_Dev_Air_Temp.show(20)
+standardAirTemp.show(20)
 
 #Find AVG air temperature per StationID in the month of February
 
@@ -94,6 +98,33 @@ avg_temps = february_data.groupBy("StationID").agg(avg("AirTemperature"))
 
 avg_temps.show(20)
 
-#adding all results in a dataframe and writing it to parquet
-df_union = Number_of_records.unionByName(average_air_temp).unionByName(median_air_temp).unionByName(Std_Dev_Air_Temp).unionByName(avg_temps)
-df_union.write.mode("append").parquet("s3a://mwaghela/MW-part-four-answers-parquet")
+
+#creating schema and writing df in parquet file
+schema1=StructType([
+StructField('Year', DateType(), True),
+StructField('Count(1)', IntegerType(), True),
+])
+
+countOfRecords.write.format('parquet').mode('overwrite').schema(schema1).save("s3a://mwaghela/MW-part-four-answers-parquet")
+
+
+schema2=StructType([
+StructField('Year', DateType(), True),
+StructField('AvgAirTemperature', DoubleType(), True),
+])
+
+averageAirTemp.write.format('parquet').mode('append').schema(schema2).save("s3a://mwaghela/MW-part-four-answers-parquet")
+
+schema3=StructType([
+StructField('Year', DateType(), True),
+StructField('Standard_deviation', DoubleType(), True),
+])
+
+standardAirTemp.write.format('parquet').mode('append').schema(schema3).save("s3a://mwaghela/MW-part-four-answers-parquet")
+
+schema4=StructType([
+StructField('StationID', StringType(), True),
+StructField('AirTemperature', DoubleType(), True),
+])
+
+avg_temps.write.format('parquet').mode('append').schema(schema4).save("s3a://mwaghela/MW-part-four-answers-parquet")
