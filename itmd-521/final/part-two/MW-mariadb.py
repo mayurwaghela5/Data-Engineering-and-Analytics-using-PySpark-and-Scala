c2d0552e50ebd9c6fe33dbc97b1975462f301856
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-04-30 22:51:34 -0500

    part 2 mariabdb added

--- itmd-521/final/part-two/MW-mariadb.py
+++ itmd-521/final/part-two/MW-mariadb.py
@@ -23,6 +23,8 @@ conf.set("fs.s3a.connection.ssl.enabled", "false")
 
 spark_session = SparkSession.builder.appName("MW-mariadb").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
 
+(splitDF.write.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","thirty").option("user",os.getenv('MYSQLUSER')).option("truncate",True).mode("overwrite").option("password", os.getenv('MYSQLPASS')).save())
+
 
 #df = spark_session.read.jdbc(url="jdbc:mysql://database-240-vm0.service.consul:3306/ncdc",table="thirties",user).load()
 df=(spark_session.read.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","thirty").option("user",os.getenv('MYSQL_USER')).option("truncate",True).option("password", os.getenv('MYSQL_PASS')).load())
