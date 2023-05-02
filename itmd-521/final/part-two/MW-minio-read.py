59116911add28e9fe73ef62981c0f09447598484
Mayur Waghela <mwaghela@hawk.iit.edu>
2023-05-02 09:57:44 -0500

    part 2 mariadb

--- itmd-521/final/part-two/MW-minio-read.py
+++ itmd-521/final/part-two/MW-minio-read.py
@@ -73,8 +73,13 @@ parquetdf.printSchema()
 print("Display PARQUETDF")
 parquetdf.show(10)
 
+#----------------MariaDB part---------------------------------------
+
+mariaDBdf = spark_session.read.csv("s3a://mwaghela/30-csv")
+
+
 #loading parrquet dataframe to Maria DB
-(cachedf.write.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","MW_thirty").option("user",os.getenv('MYSQL_USER')).option("truncate",True).mode("overwrite").option("password", os.getenv('MYSQL_PASS')).save())
+(mariaDBdf.write.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","MW_thirty").option("user",os.getenv('MYSQL_USER')).option("truncate",True).mode("overwrite").option("password", os.getenv('MYSQL_PASS')).save())
 
 
 df1=(spark_session.read.format("jdbc").option("url","jdbc:mysql://database-240-vm0.service.consul:3306/ncdc").option("driver","com.mysql.cj.jdbc.Driver").option("dbtable","MW_thirty").option("user",os.getenv('MYSQL_USER')).option("truncate",True).option("password", os.getenv('MYSQL_PASS')).load())
